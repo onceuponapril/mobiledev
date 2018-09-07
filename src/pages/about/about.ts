aa09f3a21c462c0e7f5dfc860f8ebdbbb7e4f169
zfp <zfp@umich.edu>
2018-09-06 21:20:57 -0400

    first commit

---
+++ src/pages/about/about.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+  now: Date;
+  author: string;
+
+  constructor(public navCtrl: NavController) {
+
+  this.now = new Date();
+
+  this.author = 'Fanpan Zeng'; // replace with your name
+
+
+
+  }
+
+}
